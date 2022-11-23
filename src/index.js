08c22b717e89d83777bd5714c6df53afc5ebb401
semanurakkuzu <semanurakkuzu@gmail.com>
2022-11-23 12:39:44 +0300

    added fontawesome

--- src/index.js
+++ src/index.js
@@ -6,8 +6,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
 import './index.css';
 import App from './App';
+
+library.add(fas)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
