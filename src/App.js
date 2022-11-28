9de83c21dcc76d56f119b79c8a93b2a357cd069b
semanurakkuzu <semanurakkuzu@gmail.com>
2022-11-28 11:47:06 +0300

    added dark light mode

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import wheatherCodes from "./wheatherCodes";
 
 export default function App() {
   const coordinates = {
-    Berlin: {lat:'52.510566', long:'13.38279'},
-    Paris: {lat:'48.853566', long:'2.34361'},
-    Ankara: {lat:'39.925001', long:'32.83698'},
-    Dubai: {lat:'24.991306', long:'55.171569'}
-  }
+    Berlin: { lat: "52.510566", long: "13.38279" },
+    Paris: { lat: "48.853566", long: "2.34361" },
+    Ankara: { lat: "39.925001", long: "32.83698" },
+    Dubai: { lat: "24.991306", long: "55.171569" },
+  };
 
   const wheaterTypes = wheatherCodes;
 
@@ -19,20 +19,29 @@ export default function App() {
     const url = `https://api.open-meteo.com/v1/forecast?latitude=${coordinate.lat}&longitude=${coordinate.long}&daily=weathercode,temperature_2m_max,windspeed_10m_max,shortwave_radiation_sum&timezone=Europe/Istanbul&start_date=2022-11-23&end_date=2022-11-23&hourly=relativehumidity_2m`;
 
     fetch(url)
-    .then(response => response.json())
-    .then(apiWheatherData => setWheatherData(apiWheatherData))
-    .catch(err => console.error(err))
-  }
+      .then((response) => response.json())
+      .then((apiWheatherData) => setWheatherData(apiWheatherData))
+      .catch((err) => console.error(err));
+  };
 
-  
   useEffect(() => {
-    
-    getDataFromApi(coordinates.Berlin)
-  }, [])
+    getDataFromApi(coordinates.Berlin);
+  }, []);
 
   function handleSelect(event) {
     let value = event.target.value;
     getDataFromApi(coordinates[value]);
+  }
+
+  const [theme, setTheme] = useState();
+  function handleThemeChange(e) {
+    if(e.target.checked == true) {
+      setTheme('dark-card');
+    }
+    else {
+      setTheme('');
+    }
+   
   }
 
   return (
@@ -40,14 +49,14 @@ export default function App() {
       <div className="container">
         <div className="margin-100">
           <div className="d-flex justify-content-around">
-            <div className="card card-size">
+            <div className={`card card-size ${theme}`}>
               <div className="row text-center my-3">
                 <div className="col-auto  mt-2 ms-3">
                   <select
                     className="form-select text-center"
                     aria-label="Default select example"
                     onChange={handleSelect}
-                    defaultValue={'Berlin'}
+                    defaultValue={"Berlin"}
                   >
                     <option value="Berlin">Berlin</option>
                     <option value="Paris">Paris</option>
@@ -61,6 +70,7 @@ export default function App() {
                       className="form-check-input"
                       type="checkbox"
                       id="flexSwitchCheckReverse"
+                      onChange={handleThemeChange}
                     />
                     <label
                       className="form-check-label"
@@ -71,16 +81,21 @@ export default function App() {
               </div>
               <div className="d-flex justify-content-center mt-1">
                 <div>
-                  <img src={wheatherData
-                    && wheaterTypes[wheatherData.daily.weathercode[0]].icon} alt="" className="img-size" />
+                  <img
+                    src={
+                      wheatherData &&
+                      wheaterTypes[wheatherData.daily.weathercode[0]].icon
+                    }
+                    alt=""
+                    className="img-size"
+                  />
                 </div>
               </div>
               <div>
                 <p className="fs-5 text-center">
-                  { wheatherData
-                    && wheaterTypes[wheatherData.daily.weathercode[0]].title
-                  }
-                 </p>
+                  {wheatherData &&
+                    wheaterTypes[wheatherData.daily.weathercode[0]].title}
+                </p>
               </div>
               <div className="row my-3">
                 <div className="col-auto ms-3 mt-1">
@@ -97,34 +112,29 @@ export default function App() {
                 <div className="col mt-1">
                   <div className="col">
                     <span>
-                    { wheatherData &&
-                      wheatherData.daily.windspeed_10m_max[0]
-                    }
+                      {wheatherData && wheatherData.daily.windspeed_10m_max[0]}
                     </span>
                     <span> km/h </span>
                   </div>
                   <div className="col">
                     <span>
-                    { wheatherData &&
-                      wheatherData.hourly.relativehumidity_2m[5]
-                    }</span>
+                      {wheatherData &&
+                        wheatherData.hourly.relativehumidity_2m[5]}
+                    </span>
                     <span> % </span>
                   </div>
                   <div className="col">
                     <span>
-                      { wheatherData &&
-                      wheatherData.daily.shortwave_radiation_sum[0]
-                      }
-                      </span>
+                      {wheatherData &&
+                        wheatherData.daily.shortwave_radiation_sum[0]}
+                    </span>
                     <span> MJ/m² </span>
                   </div>
                 </div>
                 <div className="col text-end me-3">
                   <span className="feels-like-text">
-                    { wheatherData &&
-                      wheatherData.daily.temperature_2m_max[0]
-                    }
-                    </span>
+                    {wheatherData && wheatherData.daily.temperature_2m_max[0]}
+                  </span>
                   <span className="feels-like-text">°</span>
                 </div>
               </div>
